274789ebdd70d61b4637805ca298efd527f5577c
nakshCoder <nakshusharm0@gmail.com>
2022-11-08 13:12:01 +0530

    My Dice Game Project

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+document.querySelector("h1").addEventListener("click", function () {
+  const randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  const randomNumber2 = Math.floor(Math.random() * 6 + 1);
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins🎉";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins 🎉";
+  } else {
+    document.querySelector("h1").textContent = "Match Draw ⛔";
+  }
+});
